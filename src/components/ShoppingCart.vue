ba0bf0f9368d4dd378d7ec1d258e66a75965126b
Gerdi <orawakene@gmail.com>
2024-11-27 16:29:34 +0200

    First shopping cart visual

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -4,24 +4,43 @@
   <div class="container mt-4">
 
     <h3 class="mb-4">Add Product</h3>
-    <form class="input-group mb-3">
-      <input class="form-control" type="text" name="name" id="name" placeholder="Product Name">
-      <input class="form-control" type="text" name="price" id="price" placeholder="Price">
-      <input class="form-control" type="text" name="quantity" id="quantity" placeholder="Quantity">
-      <button type="button" class="btn btn-outline-secondary">Add Product</button>
+    <form @submit.prevent="addProduct" class="input-group mb-3">
+      <input v-model="newProduct.name" class="form-control" type="text" name="name" id="name" placeholder="Product Name"
+             required>
+      <input v-model="newProduct.price" class="form-control" type="number" name="price" id="price" placeholder="Price"
+             required>
+      <input v-model="newProduct.quantity" class="form-control" type="number" name="quantity" id="quantity"
+             placeholder="Quantity" required>
+      <button type="submit" class="btn btn-outline-secondary">Add Product</button>
     </form>
   </div>
 
-  <div class="container mb-4 ">
-    <h3 class="mb-4">Cart Items</h3>
+  <div class="container mt-4 ">
 
-    <ul class="list-group ">
-      <li class="list-group-item d-flex justify-content-between align-items-center ">
-        Orange - 2 units @ $12
-        <button class="btn btn-danger float-right">Remove
-        </button>
-      </li>
-    </ul>
+    <table class="table">
+      <thead>
+      <tr>
+        <th>Product</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th class="text-end">Action</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in cartItems" :key="item">
+        <td>{{ item.name }}</td>
+        <td>{{ item.quantity }}</td>
+        <td>{{ item.price }}</td>
+        <td class="text-end">
+          <button @click="removeProduct(item.name)" class="btn btn-danger ">Remove
+          </button>
+        </td>
+      </tr>
+      </tbody>
+
+
+    </table>
+
 
   </div>
 
@@ -29,18 +48,18 @@
 
     <div class="d-flex justify-content-between">
       <h4>Total:</h4>
-      <h4 >${{ cartTotal.toFixed(2) }}</h4>
+      <h4>${{ cartTotal.toFixed(2) }}</h4>
     </div>
 
-      <div class="d-flex justify-content-between">
+    <div class="d-flex justify-content-between border-bottom">
       <h4>Tax:</h4>
-      <h4 >${{ cartTax.toFixed(2) }}</h4>
+      <h4>${{ cartTax.toFixed(2) }}</h4>
 
     </div>
 
     <div class="d-flex justify-content-between">
       <h3>Total with tax:</h3>
-      <h3 >${{ cartFullTotal.toFixed(2) }}</h3>
+      <h3 class="text-center">${{ cartFullTotal.toFixed(2) }}</h3>
 
     </div>
   </div>
@@ -49,67 +68,43 @@
 
 <script>
 import axios from "axios";
-//
-// export default {
-//   data: () => ({
-//     apibase: "http://localhost:8089/api/cart",
-//     cartTotal: 0,
-//     cartTax: 0,
-//     cartFullTotal:0,
-//   }),
-//   mounted() {
-//     axios
-//         .get(`${this.apibase}/total-cost`)
-//         .then((res) => (this.cartTotal = res.data))
-//         .catch(console.error),
-//         axios
-//             .get(`${this.apibase}/total-tax`)
-//             .then((res) => (this.cartTax = res.data))
-//             .catch(console.error),
-//         axios
-//             .get(`${this.apibase}/total-with-tax`)
-//             .then((res) => (this.cartFullTotal = res.data))
-//             .catch(console.error)
-//   }
-// };
 
 export default {
   data: () => ({
-    apibase: "http://localhost:8089/api/cart",
+    api: "http://localhost:8089/api/cart",
+    newProduct: {name: "", price: "", quantity: ""},
     cartTotal: 0,
     cartTax: 0,
     cartFullTotal: 0,
+    cartItems: [],
+
   }),
+
   mounted() {
-    this.fetchCartTotal();
-    this.fetchCartTax();
-    this.fetchCartFullTotal();
+    this.fetchCart();
+
   },
+
   methods: {
-    fetchCartTotal() {
-      axios
-          .get(`${this.apibase}/total-cost`)
-          .then((res) => {
-            this.cartTotal = res.data;
-          })
-          .catch(console.error);
+
+    fetchCart() {
+      axios.all([
+        axios.get(`${this.api}/all-products`).then(res => (this.cartItems = res.data)),
+        axios.get(`${this.api}/total-cost`).then(res => (this.cartTotal = res.data)),
+        axios.get(`${this.api}/total-with-tax`).then((res) => (this.cartFullTotal = res.data)),
+        axios.get(`${this.api}/total-tax`).then((res) => (this.cartTax = res.data)),
+
+      ])
     },
-    fetchCartTax() {
-      axios
-          .get(`${this.apibase}/total-tax`)
-          .then((res) => {
-            this.cartTax = res.data;
-          })
-          .catch(console.error);
+    removeProduct(name) {
+      axios.delete(`${this.api}/delete/${name}`).then(this.fetchCart);
     },
-    fetchCartFullTotal() {
-      axios
-          .get(`${this.apibase}/total-with-tax`)
-          .then((res) => {
-            this.cartFullTotal = res.data;
-          })
-          .catch(console.error);
+    addProduct() {
+      axios.post(`${this.api}/post`, this.newProduct).then(this.fetchCart);
+      this.newProduct = {name: "", price: "", quantity: ""};
     },
+
+
   },
 }
 </script>
